5ad463aa43605bcf43e8eccadb6e93294337bcd7
Jimmy Li <jimmy.jiajian.li@gmail.com>
2024-12-18 19:24:55 -0500

    add basic test framework

--- src/scripts/sheet_utilities.js
+++ src/scripts/sheet_utilities.js
@@ -1,3 +1,48 @@
-function add(a, b) {
-  return a + b;
+function transpose(matrix) {
+  return matrix[0].map((col, i) => matrix.map((row) => row[i]));
+}
+
+function getPasteSheet() {
+  return SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Paste");
+}
+
+function getHomeSheet() {
+  return SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Home");
+}
+
+function getAddressReportSheet() {
+  return SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Addresses");
+}
+
+function getTestSheet() {
+  return SpreadsheetApp.getActiveSpreadsheet().getSheetByName("TESTS");
+}
+
+function getPasteRange() {
+  return getPasteSheet().getRange("A7:K1000");
+}
+
+function getMostRecentRange() {
+  return getPasteSheet().getRange("A4");
+}
+
+function getTotalRange() {
+  return getHomeSheet().getRange("H2:K9");
+}
+
+function getDatabaseRange() {
+  return getHomeSheet().getRange("A13:M3000");
+}
+
+function getAddressReportRange() {
+  return getAddressReportSheet().getRange("A2:M3000");
+}
+
+function clearAll() {
+  SpreadsheetApp.flush();
+  getMostRecentRange().setValue("most recent visit date already added:");
+  getPasteRange().clearContent();
+  getTotalRange().setValue(0);
+  getDatabaseRange().clearContent();
+  getAddressReportRange().clearContent();
 }
--- src/tests/runner.js
+++ src/tests/runner.js
@@ -1,3 +1,5 @@
+// Because GAS debugger hangs when QUnit is loaded, all tests are written without QUnit so they can be debugged
+// Only this runner function loads QUnit to check if tests return true
 // Search for "failed" in console log to find failing tests
 function runAllTests() {
   QUnit.on("runEnd", (runEnd) => {
@@ -8,9 +10,43 @@ function runAllTests() {
 
   QUnit.start();
 
-  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Home").getRange("A1").setValue("Hello World");
+  QUnit.module("Integration", (hooks) => {
+    hooks.beforeEach(() => {
+      SpreadsheetApp.flush();
+      clearAll();
+    });
 
-  QUnit.test("two numbers", (assert) => {
-    assert.strictEqual(add(1, 2), 3);
+    hooks.afterEach(() => {
+      SpreadsheetApp.flush();
+      clearAll();
+    });
+
+    QUnit.test("testPaste", (assert) => {
+      assert.true(testPaste());
+    });
   });
+}
+
+// Given an array of values, finds the one-based index of value which matches provided name
+function findNameIndex(values, name) {
+  return values.indexOf(name) + 1;
+}
+
+// Returns range on TESTS sheet from $(testName)$(dataName) to $(End+testName)$(End+dataName)
+function loadTestData(testName, dataName) {
+  const testSheet = getTestSheet();
+  const testNames = transpose(testSheet.getRange("A:A").getValues())[0];
+  const dataNames = testSheet.getRange("1:1").getValues();
+  const dataRowStart = findNameIndex(testNames, testName);
+  const dataRowEnd = findNameIndex(testNames, `End${testName}`);
+  const dataColStart = findNameIndex(dataNames[0], dataName);
+  const dataColEnd = findNameIndex(dataNames[0], `End${dataName}`);
+
+  const dataRange = testSheet.getRange(
+    dataRowStart,
+    dataColStart,
+    dataRowEnd - dataRowStart + 1,
+    dataColEnd - dataColStart + 1,
+  );
+  return dataRange;
 }
