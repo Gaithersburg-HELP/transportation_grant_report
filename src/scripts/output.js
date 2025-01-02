988f3406734af0a7ff5fd07103884e02141020fd
Jimmy Li <jimmy.jiajian.li@gmail.com>
2025-01-01 21:55:41 -0500

    delete unused file

--- src/scripts/output.js
+++
@@ -1,8 +0,0 @@
-function toInitials(name) {
-  let initials = "";
-  const words = name.split(" ");
-  words.forEach((word) => {
-    initials += word.slice(0, 1).toUpperCase();
-  });
-  return initials;
-}
